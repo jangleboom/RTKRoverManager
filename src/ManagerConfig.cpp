a21303f211205dd691f5a17375055addfd143256
Markus Hädrich <markus.haedrich@mail.de>
2022-09-27 22:47:59 +0200

    getDeviceName updated

--- src/ManagerConfig.cpp
+++ src/ManagerConfig.cpp
@@ -14,16 +14,16 @@ uint32_t getChipId() {
 
 String getDeviceName(const String &prefix) 
 {
-  unsigned int prefixLen = prefix.length();
-  unsigned int suffixLen = 6;
-  
   String deviceName((char *)0);
   String suffix = String(getChipId(), HEX);
-  // suffix.toUpperCase();
+
+  unsigned int prefixLen = prefix.length();
+  unsigned int suffixLen = suffix.length();
   
   deviceName.reserve(prefixLen + suffixLen);
   deviceName += prefix;
   deviceName += suffix;
+
 
   return deviceName;
 }
