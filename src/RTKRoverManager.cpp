53d19d7bd449edebc35b2275c238bd9377f99dd8
Markus Hädrich <markus.haedrich@mail.de>
2023-01-25 15:23:14 +0100

    checkConnectionToWifiStation updated

--- src/RTKRoverManager.cpp
+++ src/RTKRoverManager.cpp
@@ -38,15 +38,56 @@ bool RTKRoverManager::setupStationMode(const char* ssid, const char* password, c
   return success;
 }
 
+// bool RTKRoverManager::checkConnectionToWifiStation() 
+// { 
+//   bool isConnectedToStation = false;
+
+//   if (WiFi.getMode() == WIFI_MODE_STA)
+//   {
+//     isConnectedToStation = WiFi.isConnected();
+//     // if (WiFi.status() != WL_CONNECTED) 
+//     if ( ! isConnectedToStation) 
+//     {
+//       // Check if we have credentials for a available network
+//       String ssid = readFile(LittleFS, getPath(PARAM_WIFI_SSID).c_str());
+//       String password = readFile(LittleFS, getPath(PARAM_WIFI_PASSWORD).c_str());
+//       String deviceName = getDeviceName(DEVICE_TYPE);
+//       DBG.println(F("Reconnecting to access point..."));
+//       DBG.print(F("SSID: "));
+//       DBG.println(ssid.c_str());
+//       setupStationMode(ssid.c_str(), password.c_str(), deviceName.c_str());
+//       WiFi.waitForConnectResult();
+      
+//     } 
+//     else 
+//     {
+//       DBG.print(F("WiFi connected: "));
+//       DBG.println(WiFi.SSID());
+//       DBG.println(WiFi.localIP());
+//     }
+//    }
+
+//   if (WiFi.getMode() == WIFI_MODE_AP)
+//   {
+//     DBG.print(F("Access point started: "));
+//     DBG.println(WiFi.getHostname());
+//     DBG.print(F("AP IP address: "));
+//     DBG.println(WiFi.softAPIP());
+//     DBG.print(F("AP Password: "));
+//     DBG.println(AP_PASSWORD);
+//     isConnectedToStation = false;
+//   }
+
+//   return isConnectedToStation;
+// }
+
 bool RTKRoverManager::checkConnectionToWifiStation() 
 { 
   bool isConnectedToStation = false;
 
   if (WiFi.getMode() == WIFI_MODE_STA)
   {
-    isConnectedToStation = WiFi.isConnected();
-    // if (WiFi.status() != WL_CONNECTED) 
-    if ( ! isConnectedToStation) 
+    if (! WiFi.isConnected()) 
     {
       // Check if we have credentials for a available network
       String ssid = readFile(LittleFS, getPath(PARAM_WIFI_SSID).c_str());
@@ -57,13 +98,14 @@ bool RTKRoverManager::checkConnectionToWifiStation()
       DBG.println(ssid.c_str());
       setupStationMode(ssid.c_str(), password.c_str(), deviceName.c_str());
       WiFi.waitForConnectResult();
-      
+      isConnectedToStation = WiFi.isConnected();
     } 
     else 
     {
       DBG.print(F("WiFi connected: "));
       DBG.println(WiFi.SSID());
       DBG.println(WiFi.localIP());
+      isConnectedToStation = true;
     }
   }
 
